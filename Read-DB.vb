4aaa6dab545802be468cb75ac443b9b10c456d9b
Daniel Carneiro <geral4x@gmail.com>
2016-12-16 21:51:10 +0000

    Update Read-DB.vb

--- Read-DB.vb
+++ Read-DB.vb
@@ -1,22 +1,22 @@
-Parte II
-Como ler dados de uma tabela da base de dados e carregar esses dados para um formulário.
-1.	Será necessário criar um formulário para cada tabela, do mesmo tipo dos formulários criados para o inserir. 
-2.	No código Depois da definição da classe “form” devem ser definidas as variáveis:
+
+' Como ler dados de uma tabela da base de dados e carregar esses dados para um formulário.
+' 1.	Será necessário criar um formulário para cada tabela, do mesmo tipo dos formulários criados para o inserir. 
+' 2.	No código Depois da definição da classe “form” devem ser definidas as variáveis:
 Dim strConn As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\Projectovideoclube\BaseDadosvideoclube.mdb"
-‘Conexão à base de dados
+' Conexão à base de dados
     Dim oConn As New OleDb.OleDbConnection
-‘ Variável que guardará a conexão
+'  Variável que guardará a conexão
     Dim FilmesCommand As OleDbCommand
-‘ Variável que guardará o comando SQL
+'  Variável que guardará o comando SQL
     Dim FilmesAdapter As OleDbDataAdapter
     Dim FilmesTabela As DataTable
-‘ Variaveis que guardaram os dados da Tabela no VB
+'  Variaveis que guardaram os dados da Tabela no VB
     Dim linha As OleDbDataReader
     Dim x As DataTableReader
-‘ Variáveis para apresentar os dados no formulário
+'  Variáveis para apresentar os dados no formulário
 
     Dim i As Integer 
-3.	No evento “form_load” deve ser colocado o seguinte código com as alterações necessárias para a vossa base de dados. Não esquecer de colocar os “imports” antes do inicio da classe form. 
+' 3.	No evento “form_load” deve ser colocado o seguinte código com as alterações necessárias para a vossa base de dados. Não esquecer de colocar os “imports” antes do inicio da classe form. 
     Private Sub Form5_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         
 
@@ -64,7 +64,7 @@ Dim strConn As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\Project
 
     End Sub
 
-4.	Criar um botão para navegar pelos registos da BD. Colocar no click do botão o seguinte código (Adaptado a cada tabela).
+' 4.	Criar um botão para navegar pelos registos da BD. Colocar no click do botão o seguinte código (Adaptado a cada tabela).
 If linha.Read() Then
 
             numero.Text = linha("Nfilmes")
